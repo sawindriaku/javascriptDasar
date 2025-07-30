ada890c78b3e346e1eb23a3fd19f57d782f7be4c
unknown <indrisawindri2@gmail.com>
2025-07-30 17:55:41 +0700

    latihan 1 - looping

---
+++ juraganangkot/angkot1.js
@@ -0,0 +1,7 @@
+// latihan 1
+// buat looping/perulangan bertuliskan "Angkot No. 1-10 beroperasi dengan baik."
+var angkot = 1;
+while (angkot <= 10) {
+  console.log("Angkot No. " + angkot + " beroperasi dengan baik.");
+  angkot++;
+}
