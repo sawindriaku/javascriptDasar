28105a3426c1b585708f3f4506d08d16ef355367
unknown <indrisawindri2@gmail.com>
2025-07-30 21:18:16 +0700

    latihan perulangan for dan if else

--- juraganangkot/for.js
+++ juraganangkot/for.js
@@ -2,17 +2,23 @@ for (let nilaiAwal = 1; nilaiAwal < 5; nilaiAwal++) {
   console.log("Hallo " + nilaiAwal);
 }
 
-// latihan 2 - looping menggunakan for
+// latihan 2 - looping menggunakan for dan while
 // tampilkan "angkot no 1-6 beroperasi dengan baik" dan "angkot no 7-10 sedang tidak beroperasi."
 // untuk angkot yang beroperasi dengan baik, gunakan while
 // sedangkan untuk angkot yang tidak beroperasi, gunakan for
 var noAngkot = 1;
 var jmlAngkot = 10;
-var AngkotBeroperasi = 6;
+var AngkotBeroperasi = 3;
+// perulangan while
 while (noAngkot <= AngkotBeroperasi) {
   console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
   noAngkot++;
 }
+// perulangan for
 for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
   console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi. ");
 }
+// NB: contoh lainnya
+// for (noAngkot = AngkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
+//   console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi. ");
+// }
---
+++ juraganangkot/if.js
@@ -0,0 +1,20 @@
+// var angka = prompt("Masukkan angka: ");
+// if (angka % 2 == 0) {
+//   console.log(angka + " adalah bilangan genap");
+// } else {
+//   console.log(angka + " adalah bilangan ganjil");
+// }
+
+// latihan 3 - looping menggunakan for saja
+// tampilkan "angkot no 1-6 beroperasi dengan baik" dan "angkot no 7-10 sedang tidak beroperasi."
+var noAngkot = 1;
+var jmlAngkot = 10;
+var AngkotBeroperasi = 6;
+
+for (noAngkot; noAngkot <= jmlAngkot; noAngkot++) {
+  if (noAngkot >= AngkotBeroperasi + 1) {
+    console.log("Angkot No. " + noAngkot + " sedang tidak beroperasi.");
+  } else {
+    console.log("Angkot No. " + noAngkot + " beroperasi dengan baik.");
+  }
+}
